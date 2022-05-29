dd3f89f2d6a849c765cd19d3a6ea18bf06ef825a
Kingsley Ihemere <dekings44@gmail.com>
2022-05-29 22:09:56 +0100

    refresher course

---
+++ main.py
@@ -0,0 +1,9 @@
+# Working with dictionaries and lists in python
+
+
+programming_dictionary = {
+    "Bug": "An error in a program that prevents the program from running as expected",
+    "Function": "A piece of code that you can easily call over and over again",
+    "Loop": "The action of doing somehting over and over again"
+}
+
