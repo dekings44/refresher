f867cf68f900f76789b8eb0c0a87a13c6626eb85
Kingsley Ihemere <dekings44@gmail.com>
2022-05-30 23:03:59 +0100

    grading system

---
+++ grading.py
@@ -0,0 +1,26 @@
+print('Welcome to Grading Program')
+
+student_scores = {
+    'Kingsley': 99,
+    'Chisom': 90,
+    'Chizurum': 85,
+    'Peter': 49,
+    'Chinaza': 65,
+    'Lawrence': 76
+}
+
+grade = {}
+
+for student in student_scores:
+    score = student_scores[student]
+    if score > 90:
+        student_scores[student] = "Outstanding"
+    elif score > 80:
+        student_scores[student] = "Exceeds Expectation"
+
+    elif score > 70:
+        student_scores[student] = "Acceptable"
+    else:
+        student_scores[student] = "Fail"
+
+print(student_scores)
